61b788836ed1472d0c2597159c0a6d82555e6f0e
Bebiano <claudiobebiano@gmail.com>
2019-12-31 18:40:43 +0000

    Update cron-shop.php

--- cron-shop.php
+++ cron-shop.php
@@ -61,7 +61,7 @@ else
                                     WHERE user_id = " . $user_home['user_id'])[0]["users_email"];
 
          $message = "Um ou mais dos seus servidores expiraram. Faça o login <a href='https://hlc.ovh/'>aqui</a> e vá até a loja e, em seguida, ao carrinho de compras para estender seu (s) servidor (es). <br>
-		                   <br> Para pagamento sem taxa fale com a staff antes de acabar a data <a href='https://discord.me/sfservidores'>aqui</a><br>
+		     <br> Para pagamento sem taxa fale com a staff antes de acabar a data <a href='https://discord.me/sfservidores'>aqui</a><br>
                      <br> Os seus servidores serão desativados daqui a 5 dias se não forem renovados, a factura é removida do seu carrinho, e os servidores ficam inutilizáveis.<br>                     
                      <br> Atenção não vai receber mais nenhuma notificação!!<br>
                      <br>~ <br> Obrigado!                  
